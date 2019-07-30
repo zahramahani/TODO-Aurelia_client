0db78306313ab5f032803bbd3db969b1dc28d3fb
Sara Khorashadi <sara.khorashadi@partdp.ir>
2019-07-31 01:12:00 +0430

    create new component

--- src/veiw/todo/todo.js
+++ src/veiw/todo/todo.js
@@ -1,5 +1,21 @@
+import './todo.scss'
+import {bindable} from "aurelia-framework"
+import { Task } from '../../model/task';
+
+
 export class Todo {
-  constructor() {
-    this.message = 'Hello world';
+  @bindable title;
+  @bindable done=null;
+  @bindable tasks = [];
+  newTask = 'task';
+  constructor (title) {
+    this.title= title;
   }
+  AddTask(){
+    this.tempTask= new Task(this.newTask);
+    this.tasks.push(this.tempTask);
+  }
+  setDone(done){
+    this.done = done;
+}
 }
