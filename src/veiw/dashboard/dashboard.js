bb954c6438497b8dddd1babd1210da3a5d713b80
Zahra Amirmahani <zahra.amirmahani@partdp.ir>
2019-07-30 20:52:31 +0430

    dashboard page added to dynamic pages

--- src/veiw/dashboard/dashboard.js
+++ src/veiw/dashboard/dashboard.js
@@ -1,6 +1,22 @@
-import './dashboard.scss';
+import './dashboard.scss'
+import { Board } from '../../model/board'
+// import { Todo } from '../../model/todo'
+import { User } from '../../model/user'
+
 export class Dashboard {
-  constructor() {
-    this.message = 'Hello world';
+  constructor () {
+    this.boards = []
+    this.user = new User('z', 'a')
+    this.user.setImage('pic.png')
+    this.board = new Board('board One', 'zahra')
+    this.board.addMember(this.user)
+    this.boards.push(this.board)
+  // this.boards.push(new Board("board One","zahra").addMember(new User("z","a").setImage("pic.png")))
+  // this.boards.push(new Board("board Two","sara").addMember(new user("z","a")));
+  // this.boards.push(new Board("board Three","sadegh").addMember(new user("z","a")));
+  }
+
+  setBoards (boards) {
+    this.boards = boards
   }
 }
