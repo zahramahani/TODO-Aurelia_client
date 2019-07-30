3d3a10c6eb1150f033fd383d71f433ef6962f942
Zahra Amirmahani <zahra.amirmahani@partdp.ir>
2019-07-30 12:55:53 +0430

    folders are created to seprate model and veiw

--- src/app.js
+++ src/app.js
@@ -37,12 +37,12 @@ export class App {
   configureRouter (config, router) {
     config.title = 'Aurelia'
     config.map([
-      { route: ['', 'dashboard'], name: 'dashboard', moduleId: PLATFORM.moduleName('./dashboard'), nav: true, title: 'Dashboard' },
-      { route: ['board'], name: 'board', moduleId: PLATFORM.moduleName('./board'), nav: true, title: 'Boards' },
-      { route: ['todos'], name: 'todos', moduleId: PLATFORM.moduleName('./todos'), nav: true, title: 'Todos' },
-      { route: 'not-found', name: 'not-found', moduleId: PLATFORM.moduleName('./not-found'), nav: false }
+      { route: ['', 'dashboard'], name: 'dashboard', moduleId: PLATFORM.moduleName('./veiw/dashboard/dashboard'), nav: true, title: 'Dashboard' },
+      { route: ['board'], name: 'board', moduleId: PLATFORM.moduleName('./veiw/board/board'), nav: true, title: 'Boards' },
+      { route: ['todos'], name: 'todos', moduleId: PLATFORM.moduleName('./veiw/todos/todos'), nav: true, title: 'Todos' },
+      { route: 'not-found', name: 'not-found', moduleId: PLATFORM.moduleName('./veiw/not-found/not-found'), nav: false }
     ])
-    config.fallbackRoute('board')
+    config.fallbackRoute('not-found')
     config.mapUnknownRoutes('not-found')
     this.router = router
   }
