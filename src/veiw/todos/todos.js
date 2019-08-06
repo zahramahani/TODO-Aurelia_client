f6b1b17d73a3a3f0b8b151d912d0397bc8e41dd2
Sara Khorashadi <sara.khorashadi@partdp.ir>
2019-08-06 11:45:01 +0430

    add form-group and submit to html file

--- src/veiw/todos/todos.js
+++ src/veiw/todos/todos.js
@@ -2,36 +2,46 @@ import './todos.scss'
 import { BoardModel } from '../../model/boardModel'
 import { Todo } from './../../model/todo'
 import { Task } from './../../model/task'
+// validation_part
+import {inject} from 'aurelia-dependency-injection';
+import {validationMessages} from 'aurelia-validation';
+import {ValidationControllerFactory,ValidationRules} from 'aurelia-validation';
+import {BootstrapFormRenderer} from './../../bootstrap-form-renderer';
+@inject(ValidationControllerFactory)
+// validation_part
 export class Todos {
   newTodo = '';//testchange 'todoTitle' to ''
   board;
   boards=[]
   todos = [];
   title='';
-  constructor () {
+  constructor (controllerFactory) {
     this.flag = false;
     this.board =new BoardModel('board one','zahraAmirmahani')
-    this.todo1 = new Todo('todo1')
-    this.todo2 = new Todo('todo2')
-    this.todo3 = new Todo('todo3')
-    this.task1 = new Task('task1')
-    this.task2 = new Task('task2')
-    this.task3 = new Task('task3')
-    this.todo1.addTask(this.task1)
-    this.todo2.addTask(this.task2)
-    this.todo3.addTask(this.task3)
-    this.todos.push(this.todo1)
-    this.todos.push(this.todo2)
-    this.todos.push(this.todo3)
-    this.board.addTodo(this.todo1)
-    this.board.addTodo(this.todo2)
-    this.board.addTodo(this.todo3)
-    this.boards.push(this.board)
+    this.controller = controllerFactory.createForCurrentScope();
+    this.controller.addRenderer(new BootstrapFormRenderer());
+    console.log(this.controller);
+    // this.todo1 = new Todo('todo1')
+    // this.todo2 = new Todo('todo2')
+    // this.todo3 = new Todo('todo3')
+    // this.task1 = new Task('task1')
+    // this.task2 = new Task('task2')
+    // this.task3 = new Task('task3')
+    // this.todo1.addTask(this.task1)
+    // this.todo2.addTask(this.task2)
+    // this.todo3.addTask(this.task3)
+    // this.todos.push(this.todo1)
+    // this.todos.push(this.todo2)
+    // this.todos.push(this.todo3)
+    // this.board.addTodo(this.todo1)
+    // this.board.addTodo(this.todo2)
+    // this.board.addTodo(this.todo3)
+    // this.boards.push(this.board)
 
-    this.board1 =new BoardModel('board Two','zahraAmirmahani')
-    this.board1.addTodo(this.todo1)
-    this.board1.addTodo(this.todo2)
-    this.boards.push(this.board1)
+    // this.board1 =new BoardModel('board Two','zahraAmirmahani')
+    // this.board1.addTodo(this.todo1)
+    // this.board1.addTodo(this.todo2)
+    // this.boards.push(this.board1)
   
   
   }
@@ -69,4 +79,24 @@ export class Todos {
   // y(){
   //   console.log(this.model)
   // }
+
+  submit(){
+    this.controller.validate().then(result=>{
+      if (result.valid) {
+        console.log('valid');
+        this.addTodo();
+        // this.title = 'add new task';
+        console.log(result);
+      } else {
+        console.log(result);
+      }
+    })
+    // .catch((e)=>{
+    //   console.log(e.stack);
+    // });
+  }
 }
+
+ValidationRules
+  .ensure(a => a.title).required()
+  .on(Todos);
