1b9cc652d5f90868b46d6aa35ea3ec15e40c117c
Sara Khorashadi <sara.khorashadi@partdp.ir>
2019-08-24 16:14:30 +0430

    add signup and login

--- src/veiw/signup/signup.js
+++ src/veiw/signup/signup.js
@@ -1,7 +1,25 @@
 import './signup.scss'
+import { inject } from 'aurelia-framework';
+import { HttpClient, json } from 'aurelia-fetch-client';
+import {validationMessages} from 'aurelia-validation';
+import {ValidationControllerFactory,ValidationRules} from 'aurelia-validation';
+import {BootstrapFormRenderer} from './../../bootstrap-form-renderer';
+@inject(ValidationControllerFactory,HttpClient)
 export class Signup {
+    firstName
+    lastName
+    userName
+    password
+    loginPassword
+    loginUserName
     loginFlag = true;
     signupFlag = false;
+
+    constructor (controllerFactory,httpClient) {
+        this.httpClient=httpClient;
+        this.controller = controllerFactory.createForCurrentScope();
+        this.controller.addRenderer(new BootstrapFormRenderer());
+      }
 
     login(){
         this.loginFlag = true;
@@ -13,4 +31,37 @@ export class Signup {
         this.loginFlag = false;
         this.signupFlag = true;
     }
-}
+
+    registerUser(){
+        let data = {
+            firstName:this.firstName,
+            lastName:this.lastName,
+            userName:this.userName,
+            password:this.password
+          }
+          
+          this.httpClient.fetch(`signup`, {
+            method: 'POST',
+            body: JSON.stringify(data)
+          }).then(response => response.json())
+            .then(data => {
+            console.log("regdone");
+            });
+        }
+        loginUser(){
+            let data = {
+                userName:this.loginUserName,
+                password:this.loginPassword
+            }
+            this.httpClient.fetch(`login`, {
+                method: 'POST',
+                body: JSON.stringify(data)
+              }).then(response => response.json())
+                .then(data => {
+                  console.log(data);
+                });
+        }
+    
+    }
+
+  
