11cf392743db8e1b275b6e70a8e8e7f2b1f3588a
Zahra Amirmahani <zahra.amirmahani@partdp.ir>
2019-08-25 15:48:10 +0430

    try to add unrepeated available username

--- src/veiw/board-item/board-item.js
+++ src/veiw/board-item/board-item.js
@@ -1,4 +1,4 @@
-import {bindable} from "aurelia-framework"
+import { bindable } from "aurelia-framework"
 import { User } from '../../model/user';
 import { BoardModel } from '../../model/boardModel';
 import './board-item.scss'
@@ -7,123 +7,145 @@ import { inject } from 'aurelia-framework';
 // validation_part
 
 // import { inject } from 'aurelia-framework';
-import {validationMessages} from 'aurelia-validation';
-import {ValidationControllerFactory,ValidationRules} from 'aurelia-validation';
-import {BootstrapFormRenderer} from './../../bootstrap-form-renderer';
+import { validationMessages } from 'aurelia-validation';
+import { ValidationControllerFactory, ValidationRules } from 'aurelia-validation';
+import { BootstrapFormRenderer } from './../../bootstrap-form-renderer';
 //http
 import { HttpClient, json } from 'aurelia-fetch-client';
 
 // @inject(HttpClient)
 // validation_part
-@inject(ValidationControllerFactory,HttpClient)
+@inject(ValidationControllerFactory, HttpClient)
 export class BoardItem {
   add;
- @bindable board;
- @bindable members=[];
- firstName='';
- lastName='';
- userName='';
- userId;
- 
-  constructor (controllerFactory,httpClient) {
-    this.httpClient=httpClient;
+  @bindable board;
+  @bindable members = [];
+  firstName = '';
+  lastName = '';
+  userName = '';
+  userId;
+  repeated = false;
+  enterUsername = false;
+
+  constructor(controllerFactory, httpClient) {
+    this.httpClient = httpClient;
     this.controller = controllerFactory.createForCurrentScope();
     this.controller.addRenderer(new BootstrapFormRenderer());
-    this.add=true;
- 
-    
+    this.add = true;
+
+
   }
-  attached(){
+  attached() {
     this.getOwnerName(this.board.ownerId)
     this.fetchMember()
   }
-  
-  addMember(){
-    if(this.add){
-    // this.tempMember= new User(this.userName);
-    // this.board.addMember(this.tempMember);
-    // this.userName=null;
-    // this.firstName= null;
-    // this.lastName= null;
 
-    this.httpClient.fetch('getuserIdByUserName?userName=' + this.userName)
-    .then(response => response.json())
-    .then(data => {
-      console.log("helloId")
-      console.log(data);
-     this.userId = data;
-      this.temp = {
-      boardId: this.board.boardId,
-      userId: this.userId
+  addMember() {
+    if (this.add) {
+      // this.tempMember= new User(this.userName);
+      // this.board.addMember(this.tempMember);
+      // this.userName=null;
+      // this.firstName= null;
+      // this.lastName= null;
+
+      this.httpClient.fetch('getuserIdByUserName?userName=' + this.userName)
+        .then(response => response.json())
+        .then(userIdData => {
+
+          console.log("khar")
+          console.log(userIdData);
+          console.log("khar")
+          this.httpClient.fetch('getBoardHasUser?userId='+userIdData)
+            .then(response => response.json())
+            .then(data => {
+              if (Number(data) === 0) {
+                this.repeated = false;
+                console.log("helloId")
+                console.log(data);
+                this.userId = userIdData;
+                this.temp = {
+                  boardId: this.board.boardId,
+                  userId: this.userId
+                }
+                this.httpClient.fetch(`addMemberToBoard`, {
+                  method: 'POST',
+                  body: JSON.stringify(this.temp)
+                }).then(response => response.json())
+                  .then(data => {
+                    this.fetchMember();
+                  });
+              } else {
+                console.log("repeatedd")
+                this.repeated = true;
+
+              }
+            });
+
+        }).catch((err) => {
+          this.enterUsername = true;
+          console.log('yes')
+          console.log(err)
+        });
+
+
+      // this.add = false;
+      this.userName = null;
     }
-    this.httpClient.fetch(`addMemberToBoard`, {
-      method: 'POST',
-      body: JSON.stringify(this.temp)
-    }).then(response => response.json())
-    .then(data => {
-     this.fetchMember();
-    });
-    });
-    
-    
-
-    this.add=false;
-    this.userName=null;
-  }
-  else{
-    this.add=true;
-  }
+    else {
+      // this.add = true;
+    }
 
   }
 
-  fetchMember(){
+  fetchMember() {
     this.httpClient.fetch('getMember?boardId=' + this.board.boardId)
-    .then(response => response.json())
-    .then(data => {
-      console.log('here');
-      console.log(data);
-    //  this.userId = data;
-     this.board.members = data.map(element => Object.assign(new User(), element));
-    });
-    
-  }
-
-  editBoardItem(){
+      .then(response => response.json())
+      .then(data => {
+        console.log('here');
+        console.log(data);
+        //  this.userId = data;
+        this.board.members = data.map(element => Object.assign(new User(), element));
+      });
 
   }
-  submit(){
-    if(this.add){
+
+  editBoardItem() {
+
+  }
+  submit() {
+    if (this.add) {
 
 
-    this.controller.validate().then(result=>{
-      if (result.valid) {
+      this.controller.validate().then(result => {
+        if (result.valid) {
           console.log('valid');
           this.addMember();
+
           console.log(result);
-        // this.title = 'add new task';
-      } else {
-        console.log(result);
-        // this.add=true;
-      }
-    })
-  }else{
-    this.add=true;
-  }
+          // this.title = 'add new task';
+        } else {
+          console.log(result);
+          // this.add=true;
+        }
+      })
+    } else {
+      this.add = true;
+    }
     // .catch((e)=>{
     //   console.log(e.stack);
     // });
   }
-  getOwnerName(id){
+  getOwnerName(id) {
     console.log(" in get owner name")
-      this.httpClient.fetch('userName?userId='+id)
-        .then (response => response.json())
-        .then(data => {
-          console.log(data)
-          this.board.owner=data;
-          this.temMember= new User(this.board.owner);
-          this.board.addMember(this.temMember)
-          });
-        }
+    this.httpClient.fetch('userName?userId=' + id)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.board.owner = data;
+        this.temMember = new User(this.board.owner);
+        this.board.addMember(this.temMember)
+      });
+  }
 
 
 }
