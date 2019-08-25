1ed005096ad4af00effad9fa1ab49296c4112c35
Zahra Amirmahani <zahra.amirmahani@partdp.ir>
2019-08-25 15:48:56 +0430

    logout and signup rout resolved

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import { HttpClient } from 'aurelia-fetch-client';
 import { inject } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-pal'
-@inject(HttpClient)
+import {Router} from 'aurelia-router';
+@inject(HttpClient,Router)
 export class App {
-  constructor(httpClient){
+  constructor(httpClient,router){
     this.httpClient=httpClient;
+    this.router=router;
 
+  }
+  logout(){
+    this.router.navigateToRoute('signup');
   }
   attached(){
     this.httpClient.configure(x =>{
@@ -15,11 +20,13 @@ export class App {
   configureRouter (config, router) {
     config.title = 'Aurelia'
     config.map([
-      { route: ['', 'dashboard'], name: 'dashboard', moduleId: PLATFORM.moduleName('./veiw/dashboard/dashboard'), nav: true, title: 'Dashboard' },
+      { route: ['dashboard'], name: 'dashboard', moduleId: PLATFORM.moduleName('./veiw/dashboard/dashboard'), nav: true, title: 'Dashboard' },
       { route: ['board'], name: 'board', moduleId: PLATFORM.moduleName('./veiw/board/board'), nav: true, title: 'Boards' },
       { route: ['todos'], name: 'todos', moduleId: PLATFORM.moduleName('./veiw/todos/todos'), nav: true, title: 'Todos' },
       { route: ['notFound'], name: 'notFound', moduleId: PLATFORM.moduleName('./veiw/notFound/notFound'), nav: false },
-      { route: ['signup'], name: 'signup', moduleId: PLATFORM.moduleName('./veiw/signup/signup') }
+      { route: ['', 'signup'], name: 'signup', moduleId: PLATFORM.moduleName('./veiw/signup/signup') },
+      { route: ['signup'], name: 'logout', moduleId: PLATFORM.moduleName('./veiw/signup/signup'), nav: true, title: 'logout' },
+
     ])
     // config.fallbackRoute('not-found')
     config.mapUnknownRoutes('notFound')
